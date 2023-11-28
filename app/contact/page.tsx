f1971e53cda0bf95be62f8db7c1346e7673898e1
blandsd <128013260+blandsd@users.noreply.github.com>
2023-11-28 07:55:38 +0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@#",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:validator@offellnode.top",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "validator@offellnode.top",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/blandsd",
 		label: "Github",
-		handle: "chronark",
+		handle: "offellNode",
 	},
 ];
 
